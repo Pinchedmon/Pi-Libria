55112d4a2f63183d2eefa55892b8f4e4c528e9b1
Alexey <temnikovalx@icloud.com>
2023-04-27 11:26:10 +0300

    player fixed

--- src/pages/Anime.tsx
+++ src/pages/Anime.tsx
@@ -2,12 +2,32 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import { Imovie } from "../types/Imovie";
-import { Video } from "../utils/VideoJs";
+import { VideoJS } from "../utils/VideoJs";
 
 const Anime = () => {
   const location = useLocation();
   const [movie, setMovie] = useState<Imovie>();
   const [value, setValue] = useState<any>("1");
+
+  const playerRef = React.useRef(null);
+
+  const videoJsOptions = {
+    autoplay: true,
+    controls: true,
+    responsive: true,
+    fluid: true,
+    sources: [
+      {
+        src: `https://${movie?.player.host}${movie?.player.playlist[value].hls.hd}`,
+        type: "application/x-mpegURL",
+      },
+    ],
+  };
+
+  const handlePlayerReady = (player: any) => {
+    playerRef.current = player;
+  };
+
   const play = {
     fill: true,
     fluid: true,
@@ -47,24 +67,35 @@ const Anime = () => {
     <div className="relative">
       {movie && (
         <>
-          <p> {movie?.names.ru}</p>
-          <p>{movie?.description}</p>
-          <img
-            src={`https://dl-20211030-963.anilib.top${movie?.posters.medium.url}`}
-            alt="img"
-          />
-          {playlist.at(-1) ? (
-            <>
-              <select value={value} onChange={handleChange}>
-                {playlist.map((list: any) => (
-                  <option value={list}>{list} серия</option>
-                ))}
-              </select>
-              <Video {...play} />
-            </>
-          ) : (
-            <p>Просмотр недоступен</p>
-          )}
+          <div className="bg-black/40 backdrop-blur-sm pb-4 h-[80%] rounded-2xl ml-[16px] mr-[16px]">
+            <div className="flex flex-col justify-center sm:flex-row h-full pt-6 pl-6 pr-6">
+              <div className="flex justify-center min-w-[160px] sm:min-w-[260px]">
+                <img
+                  className="min-w-[160px] rounded-xl"
+                  alt="anime"
+                  src={`https://dl-20211030-963.anilib.top/${movie?.posters.medium.url}`}
+                />
+              </div>
+              <div className="pl-4 flex flex-col justify-end text-white">
+                <p className="font-bold pb-4">{movie?.names.ru}</p>
+                <p className="font-light w-full md:mx-w-[70%] lg:max-w-[50%] xl:max-w-[35%] text-gray-200">
+                  {movie?.description}
+                </p>
+              </div>
+            </div>
+            {playlist.at(-1) ? (
+              <div className="p-6">
+                <select value={value} onChange={handleChange}>
+                  {playlist.map((list: any) => (
+                    <option value={list}>{list} серия</option>
+                  ))}
+                </select>
+                <VideoJS options={videoJsOptions} onReady={handlePlayerReady} />
+              </div>
+            ) : (
+              <p>Просмотр недоступен</p>
+            )}
+          </div>
         </>
       )}
     </div>
--- src/utils/VideoJs.tsx
+++ src/utils/VideoJs.tsx
@@ -1,30 +1,53 @@
-import React, { useEffect, useRef, useState } from "react";
+import React from "react";
 import videojs from "video.js";
 import "video.js/dist/video-js.css";
 
-export const Video = (props: any) => {
-  const videoNode = useRef(null);
-  const [player, setPlayer] = useState<any>(null);
+export const VideoJS = (props: any) => {
+  const videoRef = React.useRef(null) as any;
+  const playerRef = React.useRef(null) as any;
+  const { options, onReady } = props;
 
-  useEffect(() => {
-    if (videoNode.current) {
-      const _player = videojs(videoNode.current, props);
-      setPlayer(_player);
-      return () => {
-        if (player !== null) {
-          player!.dispose();
-        }
-      };
+  React.useEffect(() => {
+    // Make sure Video.js player is only initialized once
+    if (!playerRef.current) {
+      // The Video.js player needs to be _inside_ the component el for React 18 Strict Mode.
+      const videoElement = document.createElement("video-js");
+
+      videoElement.classList.add("vjs-big-play-centered");
+      videoRef.current.appendChild(videoElement);
+
+      const player = (playerRef.current = videojs(videoElement, options, () => {
+        videojs.log("player is ready");
+        onReady && onReady(player);
+      }));
+
+      // You could update an existing player in the `else` block here
+      // on prop change, for example:
+    } else {
+      const player = playerRef.current;
+
+      player.autoplay(options.autoplay);
+      player.src(options.sources);
     }
-  }, []);
-  useEffect(() => {
-    console.log(videoNode);
-    // const _player = videojs(videoNode.current, props);
-    // setPlayer(_player);
-  }, [props]);
+  }, [options, videoRef]);
+
+  // Dispose the Video.js player when the functional component unmounts
+  React.useEffect(() => {
+    const player = playerRef.current;
+
+    return () => {
+      if (player && !player.isDisposed()) {
+        player.dispose();
+        playerRef.current = null;
+      }
+    };
+  }, [playerRef]);
+
   return (
     <div data-vjs-player>
-      <video ref={videoNode} className="video-js"></video>
+      <div ref={videoRef} />
     </div>
   );
 };
+
+export default VideoJS;
